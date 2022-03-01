d8ec6e034cfa3e80096f9c7cdf2ba3a89a54de59
Peiying Wen <wenpeiying@gmail.com>
2022-03-01 13:58:33 -0700

    initial commit

---
+++ docs.sh
@@ -0,0 +1,5 @@
+# https://zipkin.io/pages/quickstart.html
+docker run --rm -r -p 9411:9411 --name zipkin openzipkin/zipkin
+
+npm init
+npm i --save @opentelemetry/core @opentelemetry/sdk-trace-node @opentelemetry/instrumentation-http @opentelemetry/exporter-zipkin @opentelemetry/sdk-trace-base
