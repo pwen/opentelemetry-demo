19ff37a2b9c8bde7ed1225e5ae213ae7faefdc4d
Peiying Wen <wenpeiying@gmail.com>
2022-03-01 14:04:01 -0700

    dependencies

--- docs.sh
+++ docs.sh
@@ -2,4 +2,4 @@
 docker run --rm -r -p 9411:9411 --name zipkin openzipkin/zipkin
 
 npm init
-npm i --save @opentelemetry/core @opentelemetry/sdk-trace-node @opentelemetry/instrumentation-http @opentelemetry/exporter-zipkin @opentelemetry/sdk-trace-base
+npm i --save @opentelemetry/core @opentelemetry/sdk-trace-node @opentelemetry/instrumentation-http @opentelemetry/exporter-zipkin @opentelemetry/sdk-trace-base @opentelemetry/instrumentation-express
